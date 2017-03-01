f3bfbcdb586b44bffc67eb4788cbb2d0c1d03286
Mark Williams <markzawill@gmail.com>
2017-03-01 12:41:21 -0600

    getting ready to merge files

--- src/Pickture/Models/Image.cs
+++ src/Pickture/Models/Image.cs
@@ -8,7 +8,7 @@ namespace Pickture.Models
     public class Image
     {
         public int ImageId { get; set; }
-        public string SocialMediaName { get; set; }
+        public string ImageURL { get; set; }
         public int TakerId { get; set; }
 
     }
